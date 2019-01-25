bc8fe7eddb5e27a16db783224f41e256ad69158c
Emmanuel Vargas <emmanuel.vargas-blanco@hpe.com>
2019-01-25 09:36:12 -0600

    Ejercicio de tipos de datos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Proyecto de programacion en C. CENAT 2019
+
+Tipos de datos en C
+https://www.geeksforgeeks.org/data-types-in-c/
--- src/first.c
+++ src/first.c
@@ -3,6 +3,10 @@
 int main (){
 	int dato_entero = 12;
 	char dato_caracter = 'C';
+	unsigned int
+	float
+	double
+	long double
 
 	printf("Mi dato entero es: %d, y el ocupa %lu bytes\n", dato_entero, sizeof(int));
 	printf("Mi dato caracter es: %c y el ocupa %lu bytes\n", dato_caracter, sizeof(dato_caracter));
