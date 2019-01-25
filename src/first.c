592a2341d549cadf10841da40c81e3be69724634
Emmanuel Vargas <emmanuel.vargas-blanco@hpe.com>
2019-01-25 09:32:49 -0600

    Reviso dato char

--- src/first.c
+++ src/first.c
@@ -4,7 +4,8 @@ int main (){
 	int dato_entero = 12;
 	char dato_caracter = 'C';
 
-	printf("Mi dato entero es: %d", dato_entero);
+	printf("Mi dato entero es: %d, y el ocupa %lu bytes\n", dato_entero, sizeof(int));
+	printf("Mi dato caracter es: %c y el ocupa %lu bytes\n", dato_caracter, sizeof(dato_caracter));
 
 return 0;
 }
