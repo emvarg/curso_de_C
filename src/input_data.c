ff5deecbf4bd25351e52fd7e50c632fc10136003
Emmanuel Vargas <emmanuel.vargas-blanco@hpe.com>
2019-01-25 11:14:34 -0600

    Revision de parámetro de entrada

--- src/input_data.c
+++ src/input_data.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Contador por defecto para revisión
+ * de parametros de entrada
+ */
+#define CONTADOR_POR_DEFECTO 3
+
 int main (int argc, char *argv[]){
  int i;
  int contador;
@@ -12,7 +18,12 @@ int main (int argc, char *argv[]){
    printf("Parametro de entrada %d, es : %s \n", i, argv[i]);
  }
 
+ if (argv[1] == NULL){
+	 contador = CONTADOR_POR_DEFECTO;
+ } else {
  contador = atoi(argv[1]);
+ }
+
  for(i = 0; i < contador; i++)
  {
    printf("Contador = %d \n", i);
